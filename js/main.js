2fa5ca67cfa8c37f7e0c3b16f87325d49ccdae98
Mara Rocha Fernández <maranhadisenha@gmail.com>
2021-12-02 15:38:20 +0100

    add bonus exercises

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 
 /* display new bookmark form
@@ -13,7 +13,7 @@ const menu = document.querySelector('.js-menudropdown');
 menu.classList.remove('collapsed');
 
 
-// add links
+// links info
 
 const link1_url = 'https://books.adalab.es/materiales-del-curso-n/-MdR6Gp68BX20m1pi0z2/modulo-2-programando-la-web/javascript/2_1_intro_a_la_programacion';
 const link2_url = 'https://thesmartcoder.dev/9-awesome-projects-you-can-build-with-vanilla-javascript/';
@@ -23,56 +23,85 @@ const link1_desc = 'JS en los materiales de Adalab';
 const link2_desc = 'Ideas de proyectos JS';
 const link3_desc = 'HTML en los materiales de Adalab';
 
-const link1_tag1 = 'javascript';
-const link1_tag2 = 'HTML';
+
+// BONUS: tags
+
+const link1_tag1 = ''; // javascript
+const link1_tag2 = ''; // HTML
 const link2_tag1 = 'javascript';
 const link2_tag2 = 'portfolio';
 const link3_tag1 = 'HTML';
 const link3_tag2 = 'CSS';
 
-const links_checked = 'checked';
+const no_tags = '<p class="item__tags">No tiene categorías</p>';
 
-const link1 = `<article class="data__item">
-    <p class="item__url">
+let link1_tags = `<ul class="item__tags">
+        <li class="item__tag">${link1_tag1.toLowerCase()}</li>
+        <li class="item__tag">${link1_tag2.toLowerCase()}</li>
+    </ul>`;
+
+let link2_tags = `<ul class="item__tags">
+        <li class="item__tag">${link2_tag1.toLowerCase()}</li>
+        <li class="item__tag">${link2_tag2.toLowerCase()}</li>
+    </ul>`;
+
+let link3_tags = `<ul class="item__tags">
+        <li class="item__tag">${link3_tag1.toLowerCase()}</li>
+        <li class="item__tag">${link3_tag2.toLowerCase()}</li>
+    </ul>`;
+
+if ((link1_tag1 === '') && (link1_tag2 === '')) {
+    link1_tags = no_tags;
+}
+
+if ((link2_tag1 === '') && (link2_tag2 === '')) {
+    link2_tags = no_tags;
+}
+
+if ((link3_tag1 === '') && (link3_tag2 === '')) {
+    link3_tags = no_tags;
+}
+
+
+// BONUS: checkbox
+
+const link1_checked = true;
+const link2_checked = true;
+const link3_checked = false;
+
+const checked1 = link1_checked === true ? 'checked' : '';
+const checked2 = link2_checked === true ? 'checked' : '';
+const checked3 = link3_checked === true ? 'checked' : '';
+
+
+// build links
+
+const link1 = `<p class="item__url">
         <a href="${link1_url}" target="_blank" rel="noopener noreferrer">${link1_url.replace('https://', '')}</a>
     </p>
     <p class="item__seen">
-        <input type="checkbox" ${links_checked} name="item_imp_2" id="item_imp_2">
+        <input type="checkbox" ${checked1} name="item_imp_2" id="item_imp_2">
     </p>
     <p class="item__desc">${link1_desc}</p>
-    <ul class="item__tags">
-        <li class="item__tag">${link1_tag1.toLowerCase()}</li>
-        <li class="item__tag">${link1_tag2.toLowerCase()}</li>
-    </ul>
-</article>`;
+    ${link1_tags}`;
 
-const link2 = `<article class="data__item">
-    <p class="item__url">
+const link2 = `<p class="item__url">
         <a href="${link2_url}" target="_blank">${link2_url.replace('https://','')}</a>
     </p>
     <p class="item__seen">
-        <input type="checkbox" ${links_checked} name="item_imp_1" id="item_imp_1">
+        <input type="checkbox" ${checked2} name="item_imp_1" id="item_imp_1">
     </p>
     <p class="item__desc">${link2_desc}</p>
-    <ul class="item__tags">
-        <li class="item__tag">${link2_tag1.toLowerCase()}</li>
-        <li class="item__tag">${link2_tag2.toLowerCase()}</li>
-    </ul>
-</article>`;
+    ${link2_tags}`;
 
-const link3 = `<article class="data__item">
-    <p class="item__url">
+const link3 = `<p class="item__url">
         <a href="${link3_url}" target="_blank" rel="noopener noreferrer">${link3_url.replace('https://', '')}</a>
     </p>
     <p class="item__seen">
-        <input type="checkbox" name="item_imp_2" id="item_imp_2">
+        <input type="checkbox" ${checked3} name="item_imp_2" id="item_imp_2">
     </p>
     <p class="item__desc">${link3_desc}</p>
-    <ul class="item__tags">
-        <li class="item__tag">${link3_tag1.toLowerCase()}</li>
-        <li class="item__tag">${link3_tag2.toLowerCase()}</li>
-    </ul>
-</article>`;
+    ${link3_tags}`;
 
 let html = '';
 
@@ -84,22 +113,22 @@ input_search_desc.value = 'materiales';
 const descrSearchText = input_search_desc.value;
 
 if (link1_desc.includes(descrSearchText) === true) {
-    html += `<li class="data__listitem">${link1}</li>`;
+    html += `<li class="data__listitem"><article class="data__item">${link1}</article></li>`;
 }
 
 if (link2_desc.includes(descrSearchText) === true) {
-    html += `<li class="data__listitem">${link2}</li>`;
+    html += `<li class="data__listitem"><article class="data__item">${link2}</article></li>`;
 }
 
 if (link3_desc.includes(descrSearchText) === true) {
-    html += `<li class="data__listitem">${link3}</li>`;
+    html += `<li class="data__listitem"><article class="data__item">${link3}</article></li>`;
 }
 
 const listLinks = document.querySelector('.js-data__list');
 listLinks.innerHTML = html;
 
 
-// change tableview -> listview or listview -> tableview
+// change tableview -> listview & listview -> tableview
 
 const data = document.querySelector('.js-data');
 
