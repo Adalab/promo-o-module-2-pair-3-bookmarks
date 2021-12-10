6d9d52f3e0ab01964c6c6d8abad3b5ee268720b0
Mara Rocha Fernández <maranhadisenha@gmail.com>
2021-12-10 18:17:28 +0100

    update exercise

--- js/main.js
+++ js/main.js
@@ -1,171 +1,134 @@
 'use strict';
 
 
-// links info
-
-const link1_url = 'https://books.adalab.es/materiales-del-curso-n/-MdR6Gp68BX20m1pi0z2/modulo-2-programando-la-web/javascript/2_1_intro_a_la_programacion';
-const link2_url = 'https://thesmartcoder.dev/9-awesome-projects-you-can-build-with-vanilla-javascript/';
-const link3_url = 'https://books.adalab.es/materiales-del-curso-n/-MdR6Gp68BX20m1pi0z2/modulo-1-html-y-css/1_1_intro_a_la_web';
-
-const link1_desc = 'JS en los materiales de Adalab';
-const link2_desc = 'Ideas de proyectos JS';
-const link3_desc = 'HTML en los materiales de Adalab';
-
-
-// BONUS: tags
-
-const link1_tag1 = ''; // javascript
-const link1_tag2 = ''; // HTML
-const link2_tag1 = 'javascript';
-const link2_tag2 = 'portfolio';
-const link3_tag1 = 'HTML';
-const link3_tag2 = 'CSS';
-
-const no_tags = '<p class="item__tags">No tiene categorías</p>';
-
-let link1_tags = `<ul class="item__tags">
-        <li class="item__tag">${link1_tag1.toLowerCase()}</li>
-        <li class="item__tag">${link1_tag2.toLowerCase()}</li>
-    </ul>`;
-
-let link2_tags = `<ul class="item__tags">
-        <li class="item__tag">${link2_tag1.toLowerCase()}</li>
-        <li class="item__tag">${link2_tag2.toLowerCase()}</li>
-    </ul>`;
-
-let link3_tags = `<ul class="item__tags">
-        <li class="item__tag">${link3_tag1.toLowerCase()}</li>
-        <li class="item__tag">${link3_tag2.toLowerCase()}</li>
-    </ul>`;
-
-if ((link1_tag1 === '') && (link1_tag2 === '')) {
-    link1_tags = no_tags;
-}
-
-if ((link2_tag1 === '') && (link2_tag2 === '')) {
-    link2_tags = no_tags;
-}
-
-if ((link3_tag1 === '') && (link3_tag2 === '')) {
-    link3_tags = no_tags;
+// get html element
+function getElement(selector) {
+  return document.querySelector(selector);
 }
 
 
-// BONUS: checkbox
+// bmkData objects
 
-const link1_checked = true;
-const link2_checked = true;
-const link3_checked = false;
-
-const checked1 = link1_checked === true ? 'checked' : '';
-const checked2 = link2_checked === true ? 'checked' : '';
-const checked3 = link3_checked === true ? 'checked' : '';
-
-
-// build links
-
-const link1 = `<p class="item__url">
-        <a href="${link1_url}" target="_blank" rel="noopener noreferrer">${link1_url.replace('https://', '')}</a>
-    </p>
-    <p class="item__seen">
-        <input type="checkbox" ${checked1} name="item_imp_2" id="item_imp_2">
-    </p>
-    <p class="item__desc">${link1_desc}</p>
-    ${link1_tags}`;
-
-const link2 = `<p class="item__url">
-        <a href="${link2_url}" target="_blank">${link2_url.replace('https://','')}</a>
-    </p>
-    <p class="item__seen">
-        <input type="checkbox" ${checked2} name="item_imp_1" id="item_imp_1">
-    </p>
-    <p class="item__desc">${link2_desc}</p>
-    ${link2_tags}`;
-
-const link3 = `<p class="item__url">
-        <a href="${link3_url}" target="_blank" rel="noopener noreferrer">${link3_url.replace('https://', '')}</a>
-    </p>
-    <p class="item__seen">
-        <input type="checkbox" ${checked3} name="item_imp_2" id="item_imp_2">
-    </p>
-    <p class="item__desc">${link3_desc}</p>
-    ${link3_tags}`;
-
-let html = '';
-
-
-// search "materiales"
-
-const input_search_desc = document.querySelector('.js-data-search__tag');
-input_search_desc.value = 'materiales';
-const descrSearchText = input_search_desc.value;
-
-if (link1_desc.includes(descrSearchText) === true) {
-    html += `<li class="data__listitem"><article class="data__item">${link1}</article></li>`;
+const bmkData_1 = {
+  url: 'https://books.adalab.es/materiales-del-curso-n/-MdR6Gp68BX20m1pi0z2/modulo-2-programando-la-web/javascript/2_1_intro_a_la_programacion',
+  desc: 'JS en los materiales de Adalab',
+  seen: true,
+  tag_1: '', // javascript
+  tag_2: '', // HTML
 }
 
-if (link2_desc.includes(descrSearchText) === true) {
-    html += `<li class="data__listitem"><article class="data__item">${link2}</article></li>`;
+const bmkData_2 = {
+  url: 'https://thesmartcoder.dev/9-awesome-projects-you-can-build-with-vanilla-javascript/',
+  desc: 'Ideas de proyectos JS',
+  seen: true,
+  tag_1: 'javascript',
+  tag_2: 'portfolio',
 }
 
-if (link3_desc.includes(descrSearchText) === true) {
-    html += `<li class="data__listitem"><article class="data__item">${link3}</article></li>`;
+const bmkData_3 = {
+  url: 'https://books.adalab.es/materiales-del-curso-n/-MdR6Gp68BX20m1pi0z2/modulo-1-html-y-css/1_1_intro_a_la_web',
+  desc: 'HTML en los materiales de Adalab',
+  seen: false,
+  tag_1: 'HTML',
+  tag_2: 'CSS',
 }
 
-const listLinks = document.querySelector('.js-data__list');
-listLinks.innerHTML = html;
+
+// paint html
+
+function renderTags(tag_1, tag_2) {
+  let htmlTags = '';
+
+  if (tag_1 === '' && tag_2 === '') {
+    htmlTags = '<p class="item__tags">No tiene categorías</p>';
+  } else {
+    htmlTags = `<ul class="item__tags">
+        <li class="item__tag">${tag_1.toLowerCase()}</li>
+        <li class="item__tag">${tag_2.toLowerCase()}</li>
+      </ul>`;
+  }
+
+  return htmlTags;
+}
+
+function renderBookmark(url, desc, seen, tag_1, tag_2) {
+  const htmlTags = renderTags(tag_1, tag_2);
+
+  const htmlSeen = seen ? 'checked' : '';
+
+  const htmlBookmark = `<li class="data__listitem">
+    <article class="data__item">
+      <p class="item__url">
+        <a href="${url}" target="_blank" rel="noopener noreferrer">${url.replace('https://', '')}</a>
+      </p>
+      <p class="item__seen">
+        <input type="checkbox" ${htmlSeen} name="item_imp_2" id="item_imp_2">
+      </p>
+      <p class="item__desc">${desc}</p>
+      ${htmlTags}
+    </article>
+  </li>`;
+
+  return htmlBookmark;
+}
+
+let html = renderBookmark(bmkData_1.url, bmkData_1.desc, bmkData_1.seen, bmkData_1.tag_1, bmkData_1.tag_2);
+html += renderBookmark(bmkData_2.url, bmkData_2.desc, bmkData_2.seen, bmkData_2.tag_1, bmkData_2.tag_2);
+html += renderBookmark(bmkData_3.url, bmkData_3.desc, bmkData_3.seen, bmkData_3.tag_1, bmkData_3.tag_2);
+
+const bookmarks = getElement('.js-data__list');
+bookmarks.innerHTML = html;
 
 
 // display new bookmark form
 
-const section = document.querySelector('.js-data-actions');
+const newBookmarkElement = getElement('.js-data-actions');
 
-section.addEventListener('click', showAddForm);
+newBookmarkElement.addEventListener('click', handlerClickNewBookmark);
 
-function showAddForm() {
-    section.classList.remove('hidden');
+function handlerClickNewBookmark(event) {
+  event.currentTarget.classList.remove('hidden');
 }
 
 
 // display burger menu
 
-const burgerElement = document.querySelector('.js-header__menulink');
-const menu = document.querySelector('.js-menudropdown');
+const burgerElement = getElement('.js-header__menulink');
 
-burgerElement.addEventListener('click', handlerClickLinkDropdown);
+burgerElement.addEventListener('click', handlerClickBurgerElement);
 
-function handlerClickLinkDropdown(event) {
-    event.preventDefault();
-
-    menu.classList.toggle('collapsed');
+function handlerClickBurgerElement(event) {
+  event.preventDefault();
+  const menu = getElement('.js-menudropdown');
+  menu.classList.toggle('collapsed');
 }
 
 
 // change tableview -> listview & listview -> tableview
 
-const listviewButtonElement = document.querySelector('.js-button-listview');
-const tableviewButtonElement = document.querySelector('.js-button-tableview');
-const data = document.querySelector('.js-data');
+const listviewButtonElement = getElement('.js-button-listview');
+const tableviewButtonElement = getElement('.js-button-tableview');
+const data = getElement('.js-data');
 
-listviewButtonElement.addEventListener('click', handlerListviewClick);
-tableviewButtonElement.addEventListener('click', handlerTableviewClick);
+listviewButtonElement.addEventListener('click', handlerClickListview);
+tableviewButtonElement.addEventListener('click', handlerClickTableview);
 
-function handlerListviewClick(event) {
-    event.preventDefault();
+function handlerClickListview(event) {
+  event.preventDefault();
 
-    listviewButtonElement.classList.add('selected');
-    tableviewButtonElement.classList.remove('selected');
+  listviewButtonElement.classList.add('selected');
+  tableviewButtonElement.classList.remove('selected');
 
-    data.classList.remove('tableview');
-    data.classList.add('listview');
+  data.classList.remove('tableview');
+  data.classList.add('listview');
 }
 
-function handlerTableviewClick(event) {
-    event.preventDefault();
+function handlerClickTableview(event) {
+  event.preventDefault();
 
-    tableviewButtonElement.classList.add('selected');
-    listviewButtonElement.classList.remove('selected');
+  tableviewButtonElement.classList.add('selected');
+  listviewButtonElement.classList.remove('selected');
 
-    data.classList.remove('listview');
-    data.classList.add('tableview');
+  data.classList.remove('listview');
+  data.classList.add('tableview');
 }
