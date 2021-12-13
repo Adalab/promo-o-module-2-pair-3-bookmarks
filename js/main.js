c5bbd7b8a336c6ed1f9d2b22ad9a0fdead2531be
Mara Rocha Fernández <maranhadisenha@gmail.com>
2021-12-13 11:30:30 +0100

    update exercise

--- js/main.js
+++ js/main.js
@@ -7,52 +7,49 @@ function getElement(selector) {
 }
 
 
-// bmkData objects
+// array of objects
 
-const bmkData_1 = {
-  url: 'https://books.adalab.es/materiales-del-curso-n/-MdR6Gp68BX20m1pi0z2/modulo-2-programando-la-web/javascript/2_1_intro_a_la_programacion',
-  desc: 'JS en los materiales de Adalab',
-  seen: true,
-  tag_1: '', // javascript
-  tag_2: '', // HTML
-}
-
-const bmkData_2 = {
-  url: 'https://thesmartcoder.dev/9-awesome-projects-you-can-build-with-vanilla-javascript/',
-  desc: 'Ideas de proyectos JS',
-  seen: true,
-  tag_1: 'javascript',
-  tag_2: 'portfolio',
-}
-
-const bmkData_3 = {
-  url: 'https://books.adalab.es/materiales-del-curso-n/-MdR6Gp68BX20m1pi0z2/modulo-1-html-y-css/1_1_intro_a_la_web',
-  desc: 'HTML en los materiales de Adalab',
-  seen: false,
-  tag_1: 'HTML',
-  tag_2: 'CSS',
-}
+const bmkData = [{
+    url: 'https://books.adalab.es/materiales-del-curso-n/-MdR6Gp68BX20m1pi0z2/modulo-2-programando-la-web/javascript/2_1_intro_a_la_programacion',
+    desc: 'JS en los materiales de Adalab',
+    seen: true,
+    tags: [], // 'javascript', 'HTML'
+  },
+  {
+    url: 'https://thesmartcoder.dev/9-awesome-projects-you-can-build-with-vanilla-javascript/',
+    desc: 'Ideas de proyectos JS',
+    seen: true,
+    tags: ['javascript', 'portfolio']
+  },
+  {
+    url: 'https://books.adalab.es/materiales-del-curso-n/-MdR6Gp68BX20m1pi0z2/modulo-1-html-y-css/1_1_intro_a_la_web',
+    desc: 'HTML en los materiales de Adalab',
+    seen: false,
+    tags: ['HTML', 'CSS'],
+  }
+];
 
 
 // paint html
 
-function renderTags(tag_1, tag_2) {
+function renderTags(tags) {
   let htmlTags = '';
 
-  if (tag_1 === '' && tag_2 === '') {
+  if (tags.length === 0) {
     htmlTags = '<p class="item__tags">No tiene categorías</p>';
   } else {
-    htmlTags = `<ul class="item__tags">
-        <li class="item__tag">${tag_1.toLowerCase()}</li>
-        <li class="item__tag">${tag_2.toLowerCase()}</li>
-      </ul>`;
+    htmlTags = '<ul class="item__tags">';
+    for (const tag of tags) {
+      htmlTags += `<li class="item__tag">${tag.toLowerCase()}</li>`;
+    }
+    htmlTags += '</ul>';
   }
 
   return htmlTags;
 }
 
-function renderBookmark(url, desc, seen, tag_1, tag_2) {
-  const htmlTags = renderTags(tag_1, tag_2);
+function renderBookmark(url, desc, seen, tags) {
+  const htmlTags = renderTags(tags);
 
   const htmlSeen = seen ? 'checked' : '';
 
@@ -72,9 +69,10 @@ function renderBookmark(url, desc, seen, tag_1, tag_2) {
   return htmlBookmark;
 }
 
-let html = renderBookmark(bmkData_1.url, bmkData_1.desc, bmkData_1.seen, bmkData_1.tag_1, bmkData_1.tag_2);
-html += renderBookmark(bmkData_2.url, bmkData_2.desc, bmkData_2.seen, bmkData_2.tag_1, bmkData_2.tag_2);
-html += renderBookmark(bmkData_3.url, bmkData_3.desc, bmkData_3.seen, bmkData_3.tag_1, bmkData_3.tag_2);
+let html = '';
+for (const bookmark of bmkData) {
+  html += renderBookmark(bookmark.url, bookmark.desc, bookmark.seen, bookmark.tags);
+}
 
 const bookmarks = getElement('.js-data__list');
 bookmarks.innerHTML = html;
